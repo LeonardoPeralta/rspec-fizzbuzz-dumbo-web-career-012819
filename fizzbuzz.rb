6aa1d7eef3cfa3fb984755311849c48e97d610b2
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-15 18:49:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-    if (x % 3 == 3)
+    if (x % 3 == 0)
     return 'Fizz'
-  elsif (x % 5== 5)
+  elsif (x % 5 == 0)
     return 'Buzz'
-  elsif (x % 3 == 3) && (x % 5== 5)
+  elsif (x % 3 == 0) && (x % 5 == 0)
     return 'FizzBuzz'
   else
     return x
